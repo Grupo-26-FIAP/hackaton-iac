51fbb7fa3d18dba599d2632c88f237a28e8d35f6
Jhoni Faria <jhonifarias16@gmail.com>
2025-04-26 07:27:29 -0300

    remove policy

--- s3.tf
+++ s3.tf
@@ -14,19 +14,3 @@ resource "aws_s3_bucket_public_access_block" "file_manager_bucket_public_access_
   ignore_public_acls      = false
   restrict_public_buckets = false
 }
-
-resource "aws_s3_bucket_policy" "file_manager_bucket_policy" {
-  bucket = aws_s3_bucket.file_manager_bucket.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource  = "${aws_s3_bucket.file_manager_bucket.arn}/*"
-      }
-    ]
-  })
-}
