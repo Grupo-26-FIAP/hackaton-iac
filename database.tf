8f19b790fb13e097ea2d5cc11405fca8b9f6256a
Jhoni Faria <jhonifarias16@gmail.com>
2025-04-26 07:04:10 -0300

    update database configs

--- database.tf
+++ database.tf
@@ -18,3 +18,24 @@ resource "aws_db_instance" "video_processor_db" {
     Name = "video-processor-rds"
   }
 }
+
+
+resource "aws_security_group" "database_security_group" {
+  name_prefix = "rds-sg-"
+  description = "Allow database access"
+
+  ingress {
+    description = "Allow access from ECS tasks"
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
