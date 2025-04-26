f4ac06d2bfb804fdf970648183542bba84eb1366
Jhoni Faria <jhonifarias16@gmail.com>
2025-04-26 16:56:03 -0300

    update policy

--- s3.tf
+++ s3.tf
@@ -15,19 +15,3 @@ resource "aws_s3_bucket_public_access_block" "file_manager_bucket_public_access_
   restrict_public_buckets = false
 }
 
-
-resource "aws_s3_bucket_policy" "file_manager_bucket_policy" {
-  bucket = aws_s3_bucket.file_manager_bucket.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource  = "arn:aws:s3:::hackathon-file-manager/*"
-      }
-    ]
-  })
-}
